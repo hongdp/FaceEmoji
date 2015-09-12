6618439e26582c869124fb50998441801eef0ea3
Dapeng Hong <hongdp@gmail.com>
2015-09-12 00:12:42 -0400

    capture image

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-  return render_template('index.html')
+    return render_template('index.html')
+
+@app.route("/blob", methods=['POST'])
+def handleBlob():
+    print(request.data)
+    return "Success"
 
 if __name__ == "__main__":
 	app.debug = True
